6f0851edcac3db4c6d40871fa8e01af32e9e4f86
Louie <louigiecads143@gmail.com>
2026-01-07 22:23:34 +0800

    chore: environment setup

---
+++ web/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="en">
+
+</script>
+
+<div class="flex flex-col items-center justify-center min-h-screen">
+  <h1 class="text-2xl mb-3">Welcome to Proofwork</h1>
+  <p>Find Web3 freelance jobs that fits you</p>
+</div>
+
